e4479748c5e4c108ea7475f6d875bf3bb2250f2e
yappy <superunyo@yahoo.co.jp>
2021-10-21 22:02:58 +0900

    Respond with json

--- playsv/src/main.rs
+++ playsv/src/main.rs
@@ -1,17 +1,27 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use serde::{Serialize, Deserialize};
+use serde_json;
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+#[derive(Serialize)]
+struct InfoObj {
+    description: String,
+    version: String,
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+#[get("/info")]
+async fn info() -> impl Responder {
+    let info = InfoObj {
+        description: "now testing...".to_string(),
+        version: "0.1".to_string(),
+    };
+    let body = serde_json::to_string(&info).unwrap();
+
+    HttpResponse::Ok().content_type("application/json").body(body)
 }
 
 #[actix_web::main]
@@ -19,8 +29,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .service(info)
     })
     .bind("127.0.0.1:8080")?
     .run()
